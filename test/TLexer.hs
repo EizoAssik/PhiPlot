09e6203e1a1f5ddd785c50a7b0de254b7c7df8af
NerdWizard <eizoassik@gmail.com>
2014-12-01 16:06:41 +0800

    符号格式化输出

--- test/TLexer.hs
+++ test/TLexer.hs
@@ -1,11 +1,24 @@
 module Main where
 
-import Lexer ( lexme )
+import Lexer ( lexme, Token(..) )
 import Data.Char ( toLower )
 import System.Environment ( getArgs )
+
+format_token (Name n:rs) ss = format_token rs (("Name:  " ++ n):ss)
+format_token (Real r:rs) ss = format_token rs (("Real:  " ++ (show r)):ss)
+format_token (tk:rs)     ss = format_token rs (("Token: " ++ (show tk)):ss)
+format_token []          ss = ss
+
+printListLn [] = putStrLn "Done."
+printListLn (ss:rs) = do
+    putStrLn ss
+    printListLn rs
 
 main = do
     args <- getArgs
     let fn = head args
     srcs <- readFile $ head args
-    print $ lexme srcs
+    let tokens = lexme srcs
+        tkstr  = reverse $ format_token tokens []
+    printListLn tkstr
+
