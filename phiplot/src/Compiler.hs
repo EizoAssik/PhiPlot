8587105f26ea53c87eab0c65dab0696a934249ab
NerdWizard <eizoassik@gmail.com>
2014-12-04 21:29:04 +0800

    编译器原型

---
+++ phiplot/src/Compiler.hs
@@ -0,0 +1,48 @@
+module Compiler where
+
+import Structure
+
+dump_token tk = [show tk]
+
+compile_fn (Ref "draw") =    ["DRAW"]
+compile_fn (Ref fn) =    ["CALL", ':':fn]
+compile_ref (Ref val) =  ["PUSH", '&':val]
+compile_atom (Ref val) = ["PUSH", '&':val, "LOAD"]
+compile_atom (Imm n) =   ["PUSH", show n]
+compile_atom (Subexpr expr) = compile_expr expr
+-- Hopes that the parser will never send wrong sructures...
+compile_atom (Funcall fn args) =
+    (compile_expr args) ++ (compile_fn fn)
+
+compile_expr Skip = ["NOP"]
+compile_expr (Elem atom) = compile_atom atom
+compile_expr (BinOp tk l r) = 
+    (compile_expr l) ++ (compile_expr r) ++ (dump_token tk)
+compile_expr (Logic tk l r) = 
+    (compile_expr l) ++ (compile_expr r) ++ (dump_token tk)
+compile_expr (ArgList car cdr) = 
+    (compile_expr car) ++ (compile_expr cdr)
+compile_expr (PhiList car cdr) = 
+    (compile_expr car) ++ (compile_expr cdr)
+
+compile_stmt (ExprEval expr) = compile_expr expr
+compile_stmt (Direct atom) = compile_atom atom
+compile_stmt (Return expr) = (compile_expr expr) ++ ["RET"]
+compile_stmt END = ["HALT"]
+compile_stmt NOP = ["NOP"]
+compile_stmt (Set ref expr) =
+    (compile_ref ref) ++ (compile_expr expr) ++ ["STORE"]
+compile_stmt (Block l r) =
+    (compile_stmt l) ++ (compile_stmt r)
+compile_stmt (If cond succ fail) =
+    let cond_code = compile_expr cond
+        succ_code = compile_stmt succ
+        fail_code = compile_stmt fail
+    in  cond_code ++ ["JZ", ":+"  ++ (show $ (3+) $ length succ_code)]
+                  ++ succ_code
+                  ++ ["JMP", ":+" ++ (show $ (1+) $ length fail_code)]
+                  ++ fail_code
+
+compile (Success stmt _) = compile_stmt stmt
+
+
