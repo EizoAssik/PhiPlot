50e13fe0889a2206d275d01a7066a44d27cdb417
NerdWizard <eizoassik@gmail.com>
2014-12-05 16:01:44 +0800

    支持函数调用

--- phiplot/test/TCompiler.hs
+++ phiplot/test/TCompiler.hs
@@ -3,7 +3,7 @@ module Main where
 import Lexer
 import Parser
 import Compiler
-import Preprocessor ( alloc, trans_jmp )
+import Preprocessor ( alloc, trans_jmp, trans_fcall )
 import System.Environment ( getArgs )
 import TCommon ( printListLn, format_ast )
 
@@ -13,7 +13,7 @@ main = do
     srcs <- readFile fn
     let tokens = lexme srcs
     let ast = parse tokens 
-    let fast = trans_jmp $ alloc $ foldl1 (++) $ map compile ast
+    let fast = trans_fcall $ trans_jmp $ alloc $ compile ast
     printListLn fast
 
 
