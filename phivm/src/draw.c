e29d20843db9461f41d84792fbabf23b6d5bd3d5
NerdWizard <eizoassik@gmail.com>
2014-12-07 11:33:15 +0800

    修复旋转

--- phivm/src/draw.c
+++ phivm/src/draw.c
@@ -8,11 +8,11 @@
 // static byte * pixels = (byte*) calloc(800, 800);
 static byte pixels[800*800];
 
-f64 sin_rot, cos_rot, scale_x, scale_y, org_x, org_y;
+f64 sin_rot, cos_rot=1, scale_x=1, scale_y=1, org_x, org_y;
 ui64 color;
 
 void draw() {
-    f64 x, y;
+    f64 x, y, raw_x;
     ui64 xi, yi;
     y = popv();
     x = popv();
@@ -21,8 +21,9 @@ void draw() {
 #endif
     x *= scale_x;
     y *= scale_y;
+    raw_x = x;
     x  = cos_rot * x + y * sin_rot;
-    y  = cos_rot * y - x * sin_rot;
+    y  = cos_rot * y - raw_x * sin_rot;
     x += org_x;
     y += org_y;
     xi = ((ui64) (x+400)) % 800;
